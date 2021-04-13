9becad90a9aa097e91209e149964e7c839ee8a49
Megan <carnagh5@gmail.com>
2021-04-12 20:09:56 -0400

    updated app

--- movies/src/app/app.component.spec.ts
+++ movies/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipes'`, () => {
+  it(`should have as title 'movies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipes');
+    expect(app.title).toEqual('movies');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('recipes app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('movies app is running!');
   });
 });
