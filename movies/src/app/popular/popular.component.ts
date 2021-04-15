b288bee2eccd819d127983df51d3d34b680db854
Megan <carnagh5@gmail.com>
2021-04-15 19:59:07 -0400

    got popular movies working

--- movies/src/app/popular/popular.component.ts
+++ movies/src/app/popular/popular.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+import { MoviesService } from '../movies.service';
+import { Movie } from '../interfaces/movie';
 
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
-  styleUrls: ['./popular.component.css']
+  styleUrls: ['./popular.component.css'],
 })
 export class PopularComponent implements OnInit {
+  popularMovies: Movie[] | null = null;
 
-  constructor() { }
+  constructor(private service: MoviesService) {}
 
   ngOnInit(): void {
+    this.service.getPopularMovies().subscribe((response: any) => {
+      console.log(response.results);
+      this.popularMovies = response.results;
+    });
   }
-
 }
