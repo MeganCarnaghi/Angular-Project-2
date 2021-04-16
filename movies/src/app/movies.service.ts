55b8040cc0e0549e96952ec6348a0b47d8f55b93
Megan <carnagh5@gmail.com>
2021-04-15 22:57:02 -0400

    updated now in theaters section

--- movies/src/app/movies.service.ts
+++ movies/src/app/movies.service.ts
@@ -7,12 +7,11 @@ import { HttpClient } from '@angular/common/http';
 export class MoviesService {
   apiKey: string = 'fb4b364d4422884cdbd5d864fb8cafa0';
   searchMoviesUrl: string = 'https://api.themoviedb.org/3/search/movie';
-  popularMoviesUrl: string = 'https://api.themoviedb.org/3/movie/popular';
-  upcomingMoviesUrl: string =
-    'https://api.themoviedb.org/3/search/movie/upcoming';
   movieGenreUrl: string = 'https://api.themoviedb.org/3/genre/movie/list';
   nowPlayingMoviesUrl: string =
     'https://api.themoviedb.org/3/movie/now_playing';
+  popularMoviesUrl: string = 'https://api.themoviedb.org/3/movie/popular';
+  upcomingMoviesUrl: string = 'https://api.themoviedb.org/3/movie/upcoming';
 
   constructor(private http: HttpClient) {}
 
