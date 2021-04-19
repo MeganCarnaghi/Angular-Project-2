717a169218516ec5e7952a86bdefcd5d8f154bb3
Ami Ross <amiross82792@gmail.com>
2021-04-19 18:03:42 -0400

    test

--- movies/src/app/app-routing.module.ts
+++ movies/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
+import { DescriptionComponent } from './description/description.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'watchlist', component: WatchlistPageComponent },
+  { path: 'description', component: DescriptionComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
