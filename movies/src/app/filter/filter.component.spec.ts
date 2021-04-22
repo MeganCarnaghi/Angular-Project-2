76528c8e39ad95fa3f9cb8a58ce9f6593b6b7ab7
tarverbr <67298190+tarverbr@users.noreply.github.com>
2021-04-22 19:28:01 -0400

    Filter Menu CSS

--- movies/src/app/filter/filter.component.spec.ts
+++ movies/src/app/filter/filter.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilterComponent } from './filter.component';
 
+const AngularProject2= {};
+
 describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
