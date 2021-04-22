ff7ba78ceb8756b8597ee9de105ed52d449f96ec
Megan <carnagh5@gmail.com>
2021-04-21 21:30:15 -0400

    fixed removeMovieFrom Watchlist function

--- movies/src/app/movies.service.ts
+++ movies/src/app/movies.service.ts
@@ -8,14 +8,12 @@ export class MoviesService {
   apiKey: string = 'fb4b364d4422884cdbd5d864fb8cafa0';
   searchMoviesUrl: string = 'https://api.themoviedb.org/3/search/movie';
   latestMoviesUrl: string = 'https://api.themoviedb.org/3/movie/latest';
-  popularMoviesUrl: string =
-    'https://api.themoviedb.org/3/movie/popular';
-  upcomingMoviesUrl: string =
-    'https://api.themoviedb.org/3/movie/upcoming';
+  popularMoviesUrl: string = 'https://api.themoviedb.org/3/movie/popular';
+  upcomingMoviesUrl: string = 'https://api.themoviedb.org/3/movie/upcoming';
   movieGenreUrl: string = 'https://api.themoviedb.org/3/genre/movie/list';
   nowPlayingMoviesUrl: string =
     'https://api.themoviedb.org/3/movie/now_playing';
-  
+
   watchlist: any[] = [];
 
   constructor(private http: HttpClient) {}
@@ -67,8 +65,7 @@ export class MoviesService {
     console.log(this.watchlist);
   }
 
-  removeMovieFromWatchlist(movie: any): void {
-    const index = this.watchlist.indexOf(movie);
+  removeMovieFromWatchlist(index: number): void {
     this.watchlist.splice(index, 1);
   }
 
