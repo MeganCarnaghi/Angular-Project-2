7f7b913d0d0ebfda217145fef045914ef70c4c7e
tarverbr <67298190+tarverbr@users.noreply.github.com>
2021-04-23 01:13:36 -0400

    Base

--- movies/src/app/filter/filter.component.ts
+++ movies/src/app/filter/filter.component.ts
@@ -12,33 +12,5 @@ export class FilterComponent implements OnInit {
   constructor(private movieService: MoviesService) {}
 
   ngOnInit(): void {
-  }
-
-<<<<<<< HEAD
-
-
-  
+  } 
 }
-=======
-  // FilterComponent.movielist(Rating, Genre, Length) {
-  //   this.movieService.movielist();
-  // }
-
-  // checkWatchlistForMovie(movieRef: anyRating) {
-  //   return this.movieService.checkWatchlist(movieRef);
-  // }
-
-  // checkWatchlistForMovie(movieRef: anyGenre) {
-  //   return this.movieService.checkWatchlist(movieRef);
-  // }
-
-  // checkWatchlistForMovie(movieRef: anyLength) {
-  //   return this.movieService.checkWatchlist(movieRef);
-  // }
-
-  // addToFilteredWatchList(movieRef: any) {
-  //   this.movieService.addMovieToFilteredWatchlist(movieRef);
-  //   console.log(movieRef);
-  // }
-}
->>>>>>> eecf56e8abf4e673df9743db0b6a51fab778e82b
